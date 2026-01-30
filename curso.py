4597414ddc73709f8ef7404253e8e2eafc6c2102
AlejandroM23-1 <1588479@senati.pe>
2026-01-30 13:26:52 +0000

    Update curso.py

--- curso.py
+++ curso.py
@@ -6,13 +6,3 @@ class Curso:
 
     def __str__(self):
         return f"{self.nombre} [{self.duracion} horas] {self.link}"
-
-cursos_l= [
-    Curso("Introducción a Linux", 15, "https://linux-introduction.com"),
-    Curso("Python", 43, "https://python.org"),
-    Curso("Introducción al Hacking", 53, "https://hacking-cibertec.com")
-]
-
-def lista_cursos():
-    for curso in cursos_l:
-        print(curso)
