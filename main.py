4bc9fa104bd6aaaca249010e15e2235305dc6da1
AlejandroM23-1 <1588479@senati.pe>
2026-01-30 13:27:08 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from curso import *
+def lista_cursos():
+    cursos_l= [
+    Curso("Introducción a Linux", 15, "https://linux-introduction.com"),
+    Curso("Python", 43, "https://python.org"),
+    Curso("Introducción al Hacking", 53, "https://hacking-cibertec.com")
+    ]
+    for curso in cursos_l:
+        print(curso)
 lista_cursos()
